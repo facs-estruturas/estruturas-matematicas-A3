5ad2b2315f83de3e3f4d529f265e2947bbf4cdcf
Adailton <alimasegundo@gmail.com>
2025-05-28 17:08:30 -0300

    chore: codigo adaptado para o caso do aluguel de bicicletas

--- main.jl
+++ main.jl
@@ -2,23 +2,36 @@ include("src/Markov.jl")
 using .Markov
 using Plots
 
-P = [0.9 0.1; 0.5 0.5]
-v0 = [1.0, 0.0]
+# caso aluguel de bicicletas
 
-# P = [0.0 1.0; 1.0 0.0]
-# v0 = [0.3, 0.7]
+# matriz de transicao (C, O, P)
+P = [
+    0.95  0.02  0.05;  # C → C, O, P
+    0.03  0.90  0.05;  # O → C, O, P
+    0.02  0.08  0.90   # P → C, O, P
+]
 
-passos = 100
+v0 = [0.5, # 50% centro
+      0.3, # 30% orla
+      0.2  # 20% parque
+] 
+
+passos = 20
 
 resultados = simular_cadeia(P, v0, passos)
 
 for (i, v) in enumerate(resultados)
-    println("Passo $i: ", round.(v, digits = 4))
+    println("Passo $i: ", round.(v, digits = 4), "  | Soma: ", sum(v))
 end
 
 ve = encontrar_vetor_estacionario(P)
 println("\nVetor estacionário aproximado: ", round.(ve, digits=4))
 
-plot([v[1] for v in resultados], label="Estado 1")
-plot!([v[2] for v in resultados], label="Estado 2", xlabel="Passos", ylabel="Probabilidade", title="Evolução dos estados")
+plot([v[1] for v in resultados], label="Centro", linewidth=2)
+plot!([v[2] for v in resultados], label="Orla", linewidth=2)
+plot!(
+    [v[3] for v in resultados], label="Parque", linewidth=2,
+    xlabel="Passos", ylabel="Probabilidade",
+    title="Evolução da Cadeia de Markov"
+)
 savefig("plots/evolucao.png")
--- src/Markov.jl
+++ src/Markov.jl
@@ -4,19 +4,40 @@ using LinearAlgebra
 export proxima_iteracao, simular_cadeia, encontrar_vetor_estacionario
 
 function proxima_iteracao(P::Matrix{Float64}, v::Vector{Float64})
-    return P * v
+    n = size(P, 1)
+    resultado = zeros(Float64, n)
+
+    println("\n📊 Cálculo do novo vetor (P * v):")
+    for i in 1:n
+        linha = P[i, :]
+        soma = 0.0
+        termos = []
+        for j in 1:n
+            termo = linha[j] * v[j]
+            push!(termos, "P[$i,$j]*v[$j] = $(round(linha[j], digits=3))*$(round(v[j], digits=3)) = $(round(termo, digits=6))")
+            soma += termo
+        end
+        resultado[i] = soma
+        println("→ v[$i] = ", join(termos, " + "), " = ", round(soma, digits=6))
+    end
+
+    println("\n")
+
+    return resultado
 end
 
+#simula a cadeia de markov realizando as iteracoes no array de vetores
 function simular_cadeia(P::Matrix{Float64}, v0::Vector{Float64}, passos::Int)
-    vs = [v0]
+    vs = [(v0)]
     v = v0
     for _ in 1:passos
-        v = proxima_iteracao(P, v)
+        v = (proxima_iteracao(P, v))
         push!(vs, v)
     end
     return vs
 end
 
+# tenta encontrar o vetor estacionario
 function encontrar_vetor_estacionario(P::Matrix{Float64})
     n = size(P, 1)
     A = transpose(P) - I
